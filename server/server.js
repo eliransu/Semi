d406b5e50ca781c1dfc71dd0a33f3f8ca44374dc
Eliran Suisa <elikos1@gmail.com>
2019-04-08 16:19:02 +0300

    serve build point added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const mongooseRefValidator = require('mongoose-ref-validator')
 const helmet = require('helmet')
 const routes = require('./routes')
+const path = require('path')
 
 require('dotenv').config()
 console.log(process.env.SEMI_DB_URI)
@@ -15,6 +16,16 @@ const app = express()
 app.use(helmet())
 app.use(bodyParser({ extended: true, limit: '20mb' }))
 app.use('/api', routes)
+
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('/ping', (req, res) => res.send({ ping: 'pong' }))
+app.get('/*', (req, res) => {
+  // if (req.headers.colman_private_key === process.env.COLMAN_KEY) {
+  return res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  // } else {
+  // res.status(500).end()
+  // }
+})
 
 app.listen(PORT, () => {
   console.log(`Semi server running on port: ${PORT}`)
