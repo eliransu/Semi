ed38dd4f38ca4f29fe9fa2f5fcd6a95e1b594e88
Tom Lochi <tom.lochi@gmail.com>
2019-05-18 17:42:21 +0300

    done with payment

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,91 +1,113 @@
-import { Form, Icon, Input, Button } from "antd";
-import React from "react";
-import axios from "axios";
-import AuthStore from "../../stores/AuthStore";
-import rootStores from "../../stores";
+import { Form, Icon, Input, Button, Modal } from 'antd';
+import React, { Component } from 'react';
+import axios from 'axios';
+import AuthStore from '../../stores/AuthStore';
+import rootStores from '../../stores';
+import { observer } from 'mobx-react';
 
 const authStore = rootStores[AuthStore];
 
 function hasErrors(fieldsError) {
-  return Object.keys(fieldsError).some(field => fieldsError[field]);
+	return Object.keys(fieldsError).some((field) => fieldsError[field]);
+}
+@observer
+class HorizontalLoginForm extends Component {
+	componentDidMount() {
+		// To disabled submit button at the beginning.
+		this.props.form.validateFields();
+	}
+
+	showModal = () => {
+		authStore.toggleviewLoginModal();
+	};
+
+	handleOk = (e) => {
+		console.log('okokokoko', e);
+		authStore.toggleviewLoginModal();
+	};
+
+	handleCancel = (e) => {
+		console.log(e);
+		authStore.toggleviewLoginModal();
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.form.validateFields(async (err, values) => {
+			if (!err) {
+				const res = await authStore.login(values.userName, values.password);
+
+				if (res) {
+					this.props.onLoginSuccess(authStore.getCurrentUser);
+				}
+
+				// const body = {
+				//   email:values.userName,
+				//   password:values.password
+				// }
+
+				// axios.post('api/auth/login',body)
+				// .then(res=>{
+				//   console.log(res.data.data)
+				//   if(res&&res.data&&res.data.data){
+				//     this.props.onLoginSuccess(res.data.data);
+
+				//   }
+				// })
+			}
+		});
+	};
+
+	render() {
+		const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+		// Only show error after a field is touched.
+		const userNameError = isFieldTouched('userName') && getFieldError('userName');
+		const passwordError = isFieldTouched('password') && getFieldError('password');
+		return (
+			<div className="login-main-contianer">
+				<Modal
+					style={{ width: '300px', height: '510px', borderRadius: '50px' }}
+					title="Login"
+					visible={authStore.viewLoginModal}
+					onOk={this.handleOk}
+					onCancel={this.handleCancel}
+				>
+					<Form onSubmit={this.handleSubmit}>
+						<Form.Item validateStatus={userNameError ? 'error' : ''} help={userNameError || ''}>
+							{getFieldDecorator('userName', {
+								rules: [ { required: true, message: 'Please input your username!' } ]
+							})(
+								<Input
+									prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+									placeholder="Username"
+								/>
+							)}
+						</Form.Item>
+						<Form.Item validateStatus={passwordError ? 'error' : ''} help={passwordError || ''}>
+							{getFieldDecorator('password', {
+								rules: [ { required: true, message: 'Please input your Password!' } ]
+							})(
+								<Input
+									onChange={this.onPasswordChanged}
+									prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+									type="password"
+									placeholder="Password"
+								/>
+							)}
+						</Form.Item>
+						<Form.Item>
+							<Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
+								Log in
+							</Button>
+						</Form.Item>
+					</Form>
+				</Modal>
+			</div>
+		);
+	}
 }
 
-class HorizontalLoginForm extends React.Component {
-  componentDidMount() {
-    // To disabled submit button at the beginning.
-    this.props.form.validateFields();
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields(async (err, values) => {
-      if (!err) {
-        const res = await authStore.login(values.userName, values.password);
-
-        if (res) {
-          this.props.onLoginSuccess(authStore.getCurrentUser);
-        }
-      }
-    });
-  };
-
-  render() {
-    const {
-      getFieldDecorator,
-      getFieldsError,
-      getFieldError,
-      isFieldTouched
-    } = this.props.form;
-
-    // Only show error after a field is touched.
-    const userNameError =
-      isFieldTouched("userName") && getFieldError("userName");
-    const passwordError =
-      isFieldTouched("password") && getFieldError("password");
-    return (
-      <Form layout="inline" onSubmit={this.handleSubmit}>
-        <Form.Item
-          validateStatus={userNameError ? "error" : ""}
-          help={userNameError || ""}
-        >
-          {getFieldDecorator("userName", {
-            rules: [{ required: true, message: "Please input your username!" }]
-          })(
-            <Input
-              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="Username"
-            />
-          )}
-        </Form.Item>
-        <Form.Item
-          validateStatus={passwordError ? "error" : ""}
-          help={passwordError || ""}
-        >
-          {getFieldDecorator("password", {
-            rules: [{ required: true, message: "Please input your Password!" }]
-          })(
-            <Input
-              onChange={this.onPasswordChanged}
-              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-              type="password"
-              placeholder="Password"
-            />
-          )}
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            disabled={hasErrors(getFieldsError())}
-          >
-            Log in
-          </Button>
-        </Form.Item>
-      </Form>
-    );
-  }
-}
-
-const Login = Form.create({ name: "horizontal_login" })(HorizontalLoginForm);
+const Login = Form.create({ name: 'horizontal_login' })(HorizontalLoginForm);
 
 export default Login;
--- src/components/master/Master.js
+++ src/components/master/Master.js
@@ -1,27 +1,32 @@
-import { Icon, Layout, Menu, Modal } from "antd";
-import { observer } from "mobx-react";
-import React from "react";
-import { withRouter } from "react-router";
-import { Route, Switch } from "react-router-dom";
-import Popup from "reactjs-popup";
-import rootStores from "../../stores";
-import AuthStore from "../../stores/AuthStore";
-import Login from "../Login/Login";
-import ProductInfo from "../ProductInfo/ProductInfo";
-import UserProfile from "../Store/Store";
-import About from "../about/About";
-import BecomeArenter from "../becomeArenter/BecomeArenter";
-import RegistrationSuccess from "../becomeArenter/RegistrationSeccuss";
-import Category from "../category/Category";
-import NotificationCenter from "../notification/notificationCenter";
-import PaymentPage from "../paymentPage/PaymentPage";
-import AddProductCard from "../product/AddProductCard";
-import FavoriteProductsCard from "../product/FavoriteProductsCard";
-import Product from "../product/Product";
-import Home from "./Home";
-import CategoryStore from "../../stores/CategoryStore";
-import SearchComponent from "../search/SearchComponenet";
-import OrderStore from "../../stores/OrderStore";
+import React from 'react';
+import { Layout, Menu, Icon, Modal } from 'antd';
+import { withRouter } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Carousel from '../mainHero/Carousel';
+import UserProfile from '../Store/Store';
+import About from '../about/About';
+import ProductInfo from '../ProductInfo/ProductInfo';
+import FavoriteProductsCard from '../product/FavoriteProductsCard';
+import AddProductCard from '../product/AddProductCard';
+import rootStores from '../../stores';
+import BecomeArenter from '../becomeArenter/BecomeArenter';
+import agudaImage from '../../assets/aguda.jpg';
+import colmanImage from '../../assets/colman.jpg';
+import BecomeARenter from '../becomeArenter/BecomeArenter';
+import axios from 'axios';
+import Login from '../Login/Login';
+import Product from '../product/Product';
+import Category from '../category/Category';
+import RegistrationSuccess from '../becomeArenter/RegistrationSeccuss';
+import { observer } from 'mobx-react';
+import AuthStore from '../../stores/AuthStore';
+import Popup from 'reactjs-popup';
+import NotificationCenter from '../notification/notificationCenter';
+import PaymentPage from '../paymentPage/PaymentPage';
+import CategoryStore from '../../stores/CategoryStore';
+import OrderStore from '../../stores/OrderStore';
+import SearchComponenet from '../search/SearchComponenet';
 
 const { Header, Content, Footer } = Layout;
 const authStore = rootStores[AuthStore];
@@ -30,290 +35,253 @@ const orderStore = rootStores[OrderStore];
 
 @observer
 class Master extends React.Component {
-  state = {
-    visble: false,
-    registerSuccessModal: false,
-    user: null,
-    open: false
-  };
+	state = {
+		visble: false,
+		loginVisble: false,
+		registerSuccessModal: false,
+		user: null,
+		open: false
+	};
 
-  componentDidMount() {
-    try {
-      const loggedIn = authStore.tryLogin();
-      if (!loggedIn) {
-        this.handleMenuClicked("");
-        categoryStore.init();
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
+	componentDidMount() {
+		try {
+			const loggedIn = authStore.tryLogin();
+			if (!loggedIn) {
+				this.handleMenuClicked('');
+			}
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
-  showModal = () => {
-    this.setState({ visble: true });
-  };
-  handleOk = e => {
-    //need to send data to the server
-    console.log(e);
-    this.setState({ visble: false });
-  };
+	showModal = () => {
+		this.setState({ visble: true });
+	};
+	handleOk = (e) => {
+		//need to send data to the server
+		console.log(e);
+		this.setState({ visble: false });
+	};
 
-  closeModal = () => {
-    this.setState({ open: false });
-  };
-  openModal = () => {
-    this.setState({ open: true });
-  };
+	closeModal = () => {
+		this.setState({ open: false });
+	};
+	openModal = () => {
+		this.setState({ open: true });
+	};
 
-  handleCancel = e => {
-    console.log(e);
-    this.setState({
-      visble: false
-    });
-  };
-  handleMenuClicked = path => this.props.history.push(path);
+	handleCancel = (e) => {
+		console.log(e);
+		this.setState({
+			visble: false
+		});
+	};
+	handleMenuClicked = (path) => this.props.history.push(path);
 
-  addProductClicked = () => {
-    this.setState({ registerSuccessModal: false });
-    this.handleMenuClicked("add-product-as-renter");
-  };
-  onLoginSuccess = user => {
-    if (user) {
-      this.setState({ user });
-      orderStore.loadAllOrders();	
-    }
-  };
-  onRegisterSuccess = user => {
-    if (user) {
-      this.setState({ user, visble: false, registerSuccessModal: true });
-    }
-  };
-  onCancel = () => {
-    this.setState({ visble: false });
-  };
+	addProductClicked = () => {
+		this.setState({ registerSuccessModal: false });
+		this.handleMenuClicked('add-product-as-renter');
+	};
+	onLoginSuccess = (user) => {
+		if (user) {
+			this.setState({ user });
+		}
+	};
+	onRegisterSuccess = (user) => {
+		if (user) {
+			this.setState({ user, visble: false, registerSuccessModal: true });
+		}
+	};
 
-  returnToHomePage = () => {
-    this.setState({ registerSuccessModal: false });
-  };
+	onCancel = () => {
+		this.setState({ visble: false });
+	};
 
-  render() {
-    const user = authStore.getCurrentUser;
-    console.log("user in render", user);
-    return (
-      <Layout className="layout">
-        <div className="logo" />
-        <Menu
-          theme="light"
-          mode="horizontal"
-          defaultSelectedKeys={["1"]}
-          style={{ lineHeight: "85px" }}
-        >
-          <Menu.Item
-            style={{ fontSize: 16 }}
-            key="1"
-            onClick={() => this.handleMenuClicked("")}
-          >
-            <Icon fontSize={16} type="home" />
-            Home
-          </Menu.Item>
-          <Menu.Item
-            style={{ fontSize: 16 }}
-            key="3"
-            onClick={this.showModal}
-          >
-            <Icon
-              fontSize={16}
-              style={{ marginLeft: 4 }}
-              type="notification"
-            />
-            Become A Renter!
-          </Menu.Item>
-          <Modal
-            onCancel={this.onCancel}
-            title="Become A Renter"
-            visible={this.state.visble}
-            footer={null}
-          >
-            <BecomeArenter
-              onCancelClicked={this.onCancel}
-              returnToHomePage={() =>
-                this.setState({ registerSuccessModal: false })
-              }
-              onRegistrationSuccess={this.onRegisterSuccess}
-            />
-          </Modal>
-          <Modal
-            title="Registration Seccussed"
-            visible={this.state.registerSuccessModal}
-            footer={null}
-          >
-            <RegistrationSuccess
-              returnToHomePage={this.returnToHomePage}
-              onAddProductClicked={this.addProductClicked}
-            />
-          </Modal>
-          <Menu.Item
-            style={{ fontSize: 16 }}
-            key="4"
-            onClick={() => this.handleMenuClicked("add-product-as-renter")}
-          >
-            <Icon
-              fontSize={16}
-              style={{ marginLeft: 4 }}
-              type="plus-circle"
-            />
-            Add prouct as renter!
-          </Menu.Item>
+	returnToHomePage = () => {
+		this.setState({ registerSuccessModal: false });
+	};
 
-          <Menu.Item
-            style={{ fontSize: 16 }}
-            key="6"
-            onClick={() => this.handleMenuClicked("about")}
-          >
-            <Icon fontSize={16} style={{ marginLeft: 4 }} type="team" />
-            About Us
-          </Menu.Item>
-          {user.first_name === undefined && (
-            <Menu.Item style={{ marginLeft: 70, marginBottom: 20 }} key="7">
-              {<Login onLoginSuccess={this.onLoginSuccess} />}
-            </Menu.Item>
-          )}
-          {user.first_name !== undefined && (
-            <Menu.Item
-              style={{ marginLeft: 300, marginBottom: 12 }}
-              key="7"
-            >
-              <Popup
-                trigger={
-                  <Icon
-                    type="bell"
-                    style={{
-                      color: "#e6f7ff",
-                      bordeRadius: "42px",
-                      border: "1px solid #4995e6ad",
-                      background: "#4995e6ad",
-                      fontSize: "30px",
-                      marginLeft: "10",
-                      borderRadius: "30px"
-                    }}
-                  />
-                }
-                position="left bottom"
-                defaultOpen="true"
-                contentStyle={{
-                  borderRadius: "20px",
-                  position: "absolute",
-                  zindex: "2",
-                  width: "24px",
-                  background: "rgb(255, 255, 255)",
-                  border: "1px solid rgb(187, 187, 187)",
-                  boxShadow: "rgba(0, 0, 0, 0.2) 0px 1px 3px",
-                  padding: "5px",
-                  top: "10px !important",
-                  left: "-8px !important",
-                  height: "30px"
-                }}
-              >
-                <div
-                  onClick={this.openModal}
-                  style={{
-                    position: "absolute",
-                    top: "-29px",
-                    left: "7px",
-                    fontWeight: "bold",
-                    color: "#ff8080"
-                  }}
-                >
-                  {orderStore.getallOrdersNotHendeledAsProvider.length}
-                </div>
-              </Popup>
-              <Popup
-                open={this.state.open}
-                closeOnDocumentClick
-                onClose={this.closeModal}
-                contentStyle={{
-                  borderRadius: "20px",
-                  width: "auto",
-                  position: "absolute",
-                  top: "20px",
-                  left: "67%",
-                  background: "rgb(255, 255, 255)",
-                  border: "1px solid rgb(187, 187, 187)",
-                  boxShadow: "rgba(0, 0, 0, 0.2) 0px 1px 3px"
-                }}
-              >
-                <div
-                  className="modal"
-                  style={{ display: "flex", flexDirection: "row" }}
-                >
-                  {/* <a style={{display: "flex", flexDirection: "column", justifyContent: "center", maxHeight: "18px", marginLeft: "3px", fontSize: "29px"}} onClick={this.closeModal}>
+	onLoginSuccess = (user) => {
+		this.setState({ loginVisble: false });
+	};
+	onLoginCancel = () => {
+		this.setState({ loginVisble: false });
+	};
+	showLoginModal = () => {
+		this.setState({ loginVisble: true });
+	};
+
+	render() {
+		const user = authStore.getCurrentUser;
+		console.log('user in render', user);
+		return (
+			<Layout className="layout">
+				<div className="logo" />
+				<Menu theme="light" mode="horizontal" defaultSelectedKeys={[ '1' ]} style={{ lineHeight: '85px' }}>
+					<Menu.Item style={{ fontSize: 16 }} key="1" onClick={() => this.handleMenuClicked('')}>
+						<Icon fontSize={16} type="home" />
+						Home
+					</Menu.Item>
+
+					<Menu.Item style={{ fontSize: 16 }} key="3" onClick={this.showModal}>
+						<Icon fontSize={16} style={{ marginLeft: 4 }} type="notification" />
+						Become A Renter!
+					</Menu.Item>
+					<Modal onCancel={this.onCancel} title="Become A Renter" visible={this.state.visble} footer={null}>
+						<BecomeArenter
+							onCancelClicked={this.onCancel}
+							returnToHomePage={() => this.setState({ registerSuccessModal: false })}
+							onRegistrationSuccess={this.onRegisterSuccess}
+						/>
+					</Modal>
+					<Modal title="Registration Seccussed" visible={this.state.registerSuccessModal} footer={null}>
+						<RegistrationSuccess
+							returnToHomePage={this.returnToHomePage}
+							onAddProductClicked={this.addProductClicked}
+						/>
+					</Modal>
+					<Menu.Item
+						style={{ fontSize: 16 }}
+						key="4"
+						onClick={() => this.handleMenuClicked('add-product-as-renter')}
+					>
+						<Icon fontSize={16} style={{ marginLeft: 4 }} type="plus-circle" />
+						Add prouct as renter!
+					</Menu.Item>
+
+					<Menu.Item style={{ fontSize: 16 }} key="6" onClick={() => this.handleMenuClicked('about')}>
+						<Icon fontSize={16} style={{ marginLeft: 4 }} type="team" />
+						About Us
+					</Menu.Item>
+
+					<Menu.Item style={{ fontSize: 16 }} key="7" onClick={() => this.handleMenuClicked('signin')}>
+						<Icon fontSize={16} style={{ marginLeft: 4 }} type="form" />
+						Sign in
+					</Menu.Item>
+
+					<Menu.Item style={{ fontSize: 16 }} key="8" onClick={authStore.toggleviewLoginModal}>
+						<Icon fontSize={16} style={{ marginLeft: 4 }} type="login" />
+						Log in
+					</Menu.Item>
+
+					{user.first_name === undefined && (
+						<Menu.Item style={{ marginLeft: 70, marginBottom: 20 }} key="7">
+							{<Login onLoginSuccess={this.onLoginSuccess} />}
+						</Menu.Item>
+					)}
+
+					{user.first_name !== undefined && (
+						<Menu.Item style={{ marginLeft: 300, marginBottom: 12 }} key="7">
+							<Popup
+								trigger={
+									<Icon
+										type="bell"
+										style={{
+											color: '#e6f7ff',
+											bordeRadius: '42px',
+											border: '1px solid #4995e6ad',
+											background: '#4995e6ad',
+											fontSize: '30px',
+											marginLeft: '10',
+											borderRadius: '30px'
+										}}
+									/>
+								}
+								position="left bottom"
+								defaultOpen="true"
+								contentStyle={{
+									borderRadius: '20px',
+									position: 'absolute',
+									zindex: '2',
+									width: '24px',
+									background: 'rgb(255, 255, 255)',
+									border: '1px solid rgb(187, 187, 187)',
+									boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 3px',
+									padding: '5px',
+									top: '10px !important',
+									left: '-8px !important',
+									height: '30px'
+								}}
+							>
+								<div
+									onClick={this.openModal}
+									style={{
+										position: 'absolute',
+										top: '-29px',
+										left: '7px',
+										fontWeight: 'bold',
+										color: '#ff8080'
+									}}
+								>
+									2
+								</div>
+							</Popup>
+							<Popup
+								open={this.state.open}
+								closeOnDocumentClick
+								onClose={this.closeModal}
+								contentStyle={{
+									borderRadius: '20px',
+									width: 'auto',
+									position: 'absolute',
+									top: '20px',
+									left: '67%',
+									background: 'rgb(255, 255, 255)',
+									border: '1px solid rgb(187, 187, 187)',
+									boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 3px'
+								}}
+							>
+								<div className="modal" style={{ display: 'flex', flexDirection: 'row' }}>
+									{/* <a style={{display: "flex", flexDirection: "column", justifyContent: "center", maxHeight: "18px", marginLeft: "3px", fontSize: "29px"}} onClick={this.closeModal}>
 								&times;
 							</a> */}
-                </div>
-                <div
-                  style={{
-                    fontWeight: "bold",
-                    fontSize: "16px",
-                    textAlign: "center",
-                    height: "56px"
-                  }}
-                >
-                  Notification center
-                </div>
-                <div
-                  className="separator--horizontal"
-                  style={{
-                    borderBottom: "2px solid #F2F2F2",
-                    width: "60%",
-                    padding: "2px",
-                    margin: "auto"
-                  }}
-                />
-                <NotificationCenter />
-              </Popup>
-              <span>{`,Wellcome ${user.first_name} ${
-                user.last_name
-              }`}</span>
-            </Menu.Item>
-          )}
-        </Menu>
-        <Content style={{ padding: "0 50px", backgroundColor: "#fcfcfc" }}>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/user/:userName" component={UserProfile} />
-            <Route exact path="/products" component={Product} />
-            <Route
-              exact
-              path="/products"
-              component={FavoriteProductsCard}
-            />
-            <Route
-              exact
-              path="/add-product-as-renter"
-              component={AddProductCard}
-            />
-            <Route
-              exact
-              path="/become-a-renter"
-              component={BecomeArenter}
-            />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/productPage/:id" component={ProductInfo} />
-            <Route
-              path="/category/:id"
-              history={this.props.history}
-              component={Category}
-            />
-            <Route exact path="/search" component={SearchComponent} />
-            <Route path="/paymentPage" component={PaymentPage} />
-          </Switch>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Semi Designers ©2019 Created by Semi LTD.
-        </Footer>
-      </Layout>
-    );
-  }
+								</div>
+								<div
+									style={{
+										fontWeight: 'bold',
+										fontSize: '16px',
+										textAlign: 'center',
+										height: '56px'
+									}}
+								>
+									Notification center
+								</div>
+								<div
+									className="separator--horizontal"
+									style={{
+										borderBottom: '2px solid #F2F2F2',
+										width: '60%',
+										padding: '2px',
+										margin: 'auto'
+									}}
+								/>
+								<NotificationCenter />
+							</Popup>
+							<span>{`,Wellcome ${user.first_name} ${user.last_name}`}</span>
+						</Menu.Item>
+					)}
+				</Menu>
+				<Login />
+				<Content style={{ padding: '0 50px', backgroundColor: '#fcfcfc' }}>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/user/:userName" component={UserProfile} />
+						<Route exact path="/products" component={Product} />
+						<Route exact path="/products" component={FavoriteProductsCard} />
+						<Route exact path="/add-product-as-renter" component={AddProductCard} />
+						<Route exact path="/become-a-renter" component={BecomeArenter} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/login" component={Login} />
+						<Route exact path="/productPage/:id" component={ProductInfo} />
+						<Route path="/category/:id" history={this.props.history} component={Category} />
+						<Route exact path="/paymentPage" component={PaymentPage} />
+						<Route exact path="/search" component={SearchComponenet} />
+					</Switch>
+				</Content>
+				<Footer style={{ textAlign: 'center' }}>Semi Designers ©2019 Created by Semi LTD.</Footer>
+			</Layout>
+		);
+	}
 }
 
 export default withRouter(Master);
