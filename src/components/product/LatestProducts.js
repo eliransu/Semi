67a31425a5031c413884aa83f7fa6e114a6ed355
Eliran Hasin <eliranh1@cs.colman.ac.il>
2019-08-02 11:18:53 +0300

    fix the jumping images.

--- src/components/product/LatestProducts.js
+++ src/components/product/LatestProducts.js
@@ -15,22 +15,26 @@ class LatestProducts extends Component {
   renderAllProducts = () => {
     const latestProducts = productStore.getLatestProducts;
     return latestProducts.map((product, index) => (
-      <Col span={8} style={{ padding: 30 }} key={index}>
-        <Product
-          starts={productStore.getAvargeScoreByProduct(product)}
-          history={this.props.history}
-          product={product}
+      <div style={{ paddingBottom: 50 }}>
+        <Col
+          span={8}
+          style={{ padding: 15, height: 450, marginTop: 10 }}
           key={index}
-        />
-      </Col>
+        >
+          <Product
+            starts={productStore.getAvargeScoreByProduct(product)}
+            history={this.props.history}
+            product={product}
+            key={index}
+          />
+        </Col>
+      </div>
     ));
   };
   render() {
     return (
       <Row>
-        <div className="all-products" style={{ background: "black" }}>
-          {this.renderAllProducts()}
-        </div>
+        <div className="all-products">{this.renderAllProducts()}</div>
       </Row>
     );
   }
